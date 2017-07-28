55293a0772130905090a12505973941a0154110f
Madhu Kumar D S <madhudskumar@live.in>
2017-07-29 03:59:06 +0530

    f:UI

--- dest/build.js
+++ dest/build.js
@@ -1,2 +1,18 @@
 window.onload = function () {
+    transSelectBox();
+    handleDD();
 };
+function transSelectBox() {
+    var $selectBox = document.getElementById('select-box');
+    $selectBox.classList.add('hidden');
+}
+function handleDD() {
+    var $dd = document.getElementById('dd');
+    var $li = document.getElementById('list-item').classList;
+    var _hidden = 'hide';
+    var _active = 'active';
+    $dd.addEventListener('click', function () {
+        $li.toggle(_hidden);
+        $dd.classList.toggle(_active);
+    });
+}
--- src/script/app.main.ts
+++ src/script/app.main.ts
@@ -1,3 +1,21 @@
 window.onload = () => {
-
+    transSelectBox();
+    handleDD();
 };
+
+function transSelectBox() {
+    const $selectBox = document.getElementById('select-box');
+    $selectBox.classList.add('hidden');
+}
+
+function handleDD(){
+    const $dd = document.getElementById('dd');
+    const $li = document.getElementById('list-item').classList;
+    const _hidden = 'hide';
+    const _active = 'active';
+
+    $dd.addEventListener('click', () => {
+        $li.toggle(_hidden);
+        $dd.classList.toggle(_active)
+    })
+}
